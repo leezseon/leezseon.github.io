564dfff32b536f3e3ac191581c3f0be1b1d5be1e
leezseon <leezseon@gmail.com>
2021-07-14 15:23:36 +0900

    Add custom cursor pointer.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,30 @@
 window.onload = function() {
   
+  // custom cursor
+  let mouseCursor = document.querySelector(".cursor");
+  let navLinks = document.querySelectorAll(".pointer-hover"); //메뉴 링크
+  //window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+  window.addEventListener("scroll", cursor);
+  window.addEventListener("mousemove", cursor);
+  //커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
+  function cursor(e) {
+    mouseCursor.style.left = e.pageX + "px";
+    mouseCursor.style.top = e.pageY - scrollY + "px";
+  }
+
+  navLinks.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("pointer");
+      mouseCursor.style.zIndex = "9000";
+      link.classList.add("pointer-hover");
+    });
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("pointer");
+      mouseCursor.style.zIndex = "9000";
+      link.classList.remove("pointer-hover");
+    });
+  });
+
   // logo-home
   const homeBtn = document.getElementsByClassName("logo-home");
   const homeBtnHov = document.getElementsByClassName("logo-home-hover")
